fa6d9de68d19e186aed84aa8afeac7ba0c3e9b34
chakyl38 <kylechapman09@gmail.com>
2024-07-14 21:05:07 -0500

    about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+        <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+            Voluptate dolorum ullam quod sunt quasi eius iure dolorem earum, ipsa quia reprehenderit ad laboriosam corrupti! 
+            Aspernatur quaerat eveniet nesciunt voluptatem labore!
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Ipsa, mollitia. In dolorem nobis fuga quae? 
+            Voluptate commodi quo eum illum nostrum sint voluptates autem quam soluta blanditiis, excepturi perferendis ipsum!
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Accusantium rem, repellendus error, quibusdam dolore ut pariatur at consectetur voluptate ratione repudiandae voluptas, iusto velit. 
+            Numquam, in porro. Labore, non sit. Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Aperiam blanditiis modi quia rerum illo error pariatur qui omnis. 
+            Quaerat ipsum aut eveniet rem voluptate ad explicabo odit quidem quasi magnam
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Blanditiis ex incidunt vero, dolor, doloremque temporibus officia, sit est consectetur sunt quo adipisci reprehenderit porro unde debitis tenetur nam aut id?
+        </p>
+    </div>
+  )
+}
